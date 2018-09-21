a810236428f78569bfe72f44716506c76cc7ad36
Trac Nguyen <trac.nguyen@news.com.au>
2018-09-21 22:54:28 +0700

    Add composer

---
+++ src/WpTheme.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tracnguyen
+ * Date: 9/21/18
+ * Time: 10:52 PM
+ */
+
+namespace Enpii\Wp\EnpiiSample2018;
+
+
+class WpTheme extends \Enpii\Wp\EnpiiBase\Component\WpTheme {
+{
+
+}
